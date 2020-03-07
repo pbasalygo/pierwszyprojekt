92eb2d88b55ff27192c1f713fc4727e6af684a6b
pbasalygo <p.basalygo@student.po.edu.pl>
2020-03-07 14:35:05 +0100

    second commit

--- ProjectAPI/Startup.cs
+++ ProjectAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace ProjectAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
